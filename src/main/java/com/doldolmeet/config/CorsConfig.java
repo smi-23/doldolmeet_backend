c0f2c4037b63f6c06888c72f81b91ac61152d838
SangEon <86558575+sangeon-ahn@users.noreply.github.com>
2023-11-21 01:28:11 +0900

    feat: CORS 설정 config 추가

---
+++ src/main/java/com/doldolmeet/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.doldolmeet.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        // 모든 도메인 및 모든 HTTP 메소드에 대한 허용을 설정합니다.
+        config.addAllowedOrigin("https://www.doldolmeet.shop");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
+
