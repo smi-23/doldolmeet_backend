4ba8bcd837990e66f61de6955a5e8281ce91e390
smi-23 <latack789@naver.com>
2023-11-22 03:55:27 +0900

    Feat: fanMeetingUrlImg업로드를 위해 임시로 aws수정

--- src/main/java/com/doldolmeet/s3/service/AwsS3Service.java
+++ src/main/java/com/doldolmeet/s3/service/AwsS3Service.java
@@ -72,6 +72,28 @@ public class AwsS3Service {
         return fileNameList;
     }
 
+    public List<String> uploadFilebeforefanmeeting(List<MultipartFile> multipartFile) {
+        List<String> fileNameList = new ArrayList<>();
+
+        // forEach 구문을 통해 multipartFile로 넘어온 파일들 하나씩 fileNameList에 추가
+        multipartFile.forEach(file -> {
+            String fileName = createFileName(file.getOriginalFilename());
+            ObjectMetadata objectMetadata = new ObjectMetadata();
+            objectMetadata.setContentLength(file.getSize());
+            objectMetadata.setContentType(file.getContentType());
+
+            try(InputStream inputStream = file.getInputStream()) {
+                amazonS3.putObject(new PutObjectRequest(bucket, fileName, inputStream, objectMetadata)
+                        .withCannedAcl(CannedAccessControlList.PublicRead));
+            } catch(IOException e) {
+                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "파일 업로드에 실패했습니다.");
+            }
+            fileNameList.add(fileName);
+        });
+
+        return fileNameList;
+    }
+
     /**
      * AWS S3 버킷에서 파일을 다운로드하는 메서드.
      *
