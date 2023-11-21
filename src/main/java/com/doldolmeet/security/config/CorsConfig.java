5a105e5ab07ba8364ea3353f15b6596a4a19d6a1
SangEon <86558575+sangeon-ahn@users.noreply.github.com>
2023-11-21 22:08:09 +0900

    chore: CORS config 위치를 security 패키지로 변경

--- src/main/java/com/doldolmeet/security/config/CorsConfig.java
+++ src/main/java/com/doldolmeet/security/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.doldolmeet.config;
+package com.doldolmeet.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
