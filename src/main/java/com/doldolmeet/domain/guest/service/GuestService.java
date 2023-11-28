263168b492f187db9444fcea6edab1ba1b8029a1
SangEon <86558575+sangeon-ahn@users.noreply.github.com>
2023-11-28 14:06:12 +0900

    feat: jwt 토큰에 nickname정보 추가

--- src/main/java/com/doldolmeet/domain/guest/service/GuestService.java
+++ src/main/java/com/doldolmeet/domain/guest/service/GuestService.java
@@ -130,7 +130,7 @@ public class GuestService {
                 throw new CustomException(INVALID_PASSWORD);
             }
 
-            jwtToken = jwtUtil.createToken(username, fan.get().getUserCommons().getRole());
+            jwtToken = jwtUtil.createToken(username, fan.get().getUserCommons().getRole(), fan.get().getUserCommons().getNickname());
         }
 
         else if (idol.isPresent()) {
@@ -138,7 +138,7 @@ public class GuestService {
                 throw new CustomException(INVALID_PASSWORD);
             }
 
-            jwtToken = jwtUtil.createToken(username, idol.get().getUserCommons().getRole());
+            jwtToken = jwtUtil.createToken(username, idol.get().getUserCommons().getRole(), idol.get().getUserCommons().getNickname());
         }
 
         else if (admin.isPresent()) {
@@ -146,7 +146,7 @@ public class GuestService {
                 throw new CustomException(INVALID_PASSWORD);
             }
 
-            jwtToken = jwtUtil.createToken(username, admin.get().getUserCommons().getRole());
+            jwtToken = jwtUtil.createToken(username, admin.get().getUserCommons().getRole(), admin.get().getUserCommons().getNickname());
         }
 
         else {
